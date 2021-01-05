c3204fe412aec788f4d6d65f201481c326f1ae97
t_honma <toru.hom09@gmail.com>
2021-01-05 09:37:26 +0900

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    isDisabled = true;
 
-  constructor() {}
+    constructor() {}
 
+    async scrolling($event) {
+         //実際のスクロールが行われる要素を取得
+        const scrollElement = await $event.target.getScrollElement();
+
+         //スクロールの全体の高さ
+        const scrollHeight = scrollElement.scrollHeight - scrollElement.clientHeight;
+
+         //今のscroll位置
+        const currentScrollDepth = scrollElement.scrollTop;
+
+         if(currentScrollDepth == scrollHeight) {
+            console.log('scrolled!!');
+            this.isDisabled = false;
+
+         }
+    }
 }
